0d1fedd7b82c8959cfceec39f0d95bbed6a0f4ca
Usairam-s <muhammaduseram@gmail.com>
2024-05-08 00:41:00 +0500

    add auth

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,12 @@
+"use client";
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
 
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-col gap-4">
       <Link href="/">
@@ -16,9 +19,27 @@ function Sidebar() {
         <HiHome className="w-7 h-7" />
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 text-white rounded-full hover:bg-brightness-95 transition-all duration-200 h-9 w-48 shadow-lg hidden xl:inline">
-        Sign in
-      </button>
+      {session ? (
+        <>
+          {" "}
+          <button
+            className="bg-blue-400 text-white rounded-full hover:bg-brightness-95 transition-all duration-200 h-9 w-48 shadow-lg hidden xl:inline"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <button
+            className="bg-blue-400 text-white rounded-full hover:bg-brightness-95 transition-all duration-200 h-9 w-48 shadow-lg hidden xl:inline"
+            onClick={() => signIn()}
+          >
+            Sign in
+          </button>
+        </>
+      )}
     </div>
   );
 }
