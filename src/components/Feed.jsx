4b4f932a8557e9792711a8b169c4dad373305dc7
Usairam-s <muhammaduseram@gmail.com>
2024-05-11 23:33:47 +0500

    fix auto refresh afer new post add

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,25 +1,38 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import {
   collection,
   getDocs,
   getFirestore,
   orderBy,
   query,
+  onSnapshot,
 } from "firebase/firestore";
 import { app } from "@/firebase";
 import Post from "./Post";
 
-export default async function Feed() {
-  const db = getFirestore(app);
-  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
-  const querySnapshot = await getDocs(q);
-  let data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({ id: doc.id, ...doc.data() });
-  });
+export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const db = getFirestore(app);
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const newPosts = [];
+      snapshot.forEach((doc) => {
+        newPosts.push({ id: doc.id, ...doc.data() });
+      });
+      setPosts(newPosts);
+    });
+
+    // Cleanup function
+    return () => unsubscribe();
+  }, []); // Empty dependency array to run the effect only once
 
   return (
     <div>
-      {data.map((post) => (
+      {posts.map((post) => (
         <Post key={post.id} post={post} id={post.id} />
       ))}
     </div>
